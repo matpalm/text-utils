442d3ce9906b2d58f5f3bbf21a14ab25aca19d09
mat kelcey <matthew.kelcey@gmail.com>
2012-03-27 22:10:41 -0700

    token document freq

---
+++ src/token_document_freq.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <set>
+#include <map>
+#include <cstdlib>
+
+using namespace std;
+
+typedef set<string> ss;
+typedef map<string,int> msi;
+
+int main(int argc, char **argv)
+{
+
+    msi freqs;
+
+    string line;
+    while (getline(cin, line))
+    {
+        // unique the tokens
+        istringstream iss(line, istringstream::in);
+        ss unique_tokens;
+        string token;
+        while (iss >> token)
+            unique_tokens.insert(token);
+        // add unique tokens to freq table
+        for (ss::iterator it=unique_tokens.begin(); it!=unique_tokens.end(); ++it)
+            ++freqs[*it];
+    }
+
+    // dump
+    for(msi::iterator it=freqs.begin(); it!=freqs.end(); ++it)
+        cout << it->first << "\t" << it->second << "\n";
+}
