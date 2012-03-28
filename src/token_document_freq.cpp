5cf5f950c57c394586da466227a92f6d99812bb1
mat kelcey <matthew.kelcey@gmail.com>
2012-03-27 22:18:31 -0700

    std::copy to set

--- src/token_document_freq.cpp
+++ src/token_document_freq.cpp
@@ -5,6 +5,7 @@
 #include <set>
 #include <map>
 #include <cstdlib>
+#include <iterator>
 
 using namespace std;
 
@@ -13,23 +14,20 @@ typedef map<string,int> msi;
 
 int main(int argc, char **argv)
 {
-
     msi freqs;
-
     string line;
     while (getline(cin, line))
     {
         // unique the tokens
         istringstream iss(line, istringstream::in);
         ss unique_tokens;
-        string token;
-        while (iss >> token)
-            unique_tokens.insert(token);
+        copy(istream_iterator<string>(iss),
+             istream_iterator<string>(),
+             inserter(unique_tokens, unique_tokens.begin()));
         // add unique tokens to freq table
         for (ss::iterator it=unique_tokens.begin(); it!=unique_tokens.end(); ++it)
             ++freqs[*it];
     }
-
     // dump
     for(msi::iterator it=freqs.begin(); it!=freqs.end(); ++it)
         cout << it->first << "\t" << it->second << "\n";
